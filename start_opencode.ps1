76796ffb286a3e549fde9a4fed8b9dc30e152e52
Francisco <javmar71@gmail.com>
2026-09-20 00:50:16 -0500

    chore: retira lanzadores del repositorio (se alojan en mi_proyecto_finanzas, fuera del control de versiones)

--- start_opencode.ps1
+++
@@ -1,8 +0,0 @@
-# Ir al directorio del proyecto
-cd C:\Users\Usuario\Desktop\mi_proyecto_finanzas
-
-# Activar el entorno virtual venv
-& "C:\Users\Usuario\Desktop\mi_proyecto_finanzas\venv\Scripts\Activate.ps1"
-
-# Lanzar OpenCode en modo interactivo
-opencode
